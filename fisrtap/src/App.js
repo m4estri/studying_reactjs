54dea22d65b0f3cdc410d9186dd6f4968afb7ff9
Isaque Maestri <ismaestri@outlook.com>
2019-10-08 12:58:41 -0300

    stylesheet

--- fisrtap/src/App.js
+++ fisrtap/src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import './style.css'
+
 
 class App extends Component {
     render(){
         return(
-            <div>
-                Welcome to the hell!
+            <div className="container">
+               <img src={require('./assets/cronometro.png')} className="img"/>
+               <a className="timer">0.0</a>
+            <div className="areaBtn">
+                <a className="btn">INICIAR</a>
+                <a className="btn">ZERAR</a>
+            </div>
             </div>
         )
     }
