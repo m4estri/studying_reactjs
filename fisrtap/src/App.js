ce6c5b20d6d76046850f2b258b96933fa9757419
Isaque Maestri <ismaestri@outlook.com>
2019-10-08 12:26:46 -0300

    first commit

--- fisrtap/src/App.js
+++ fisrtap/src/App.js
@@ -1,43 +1,13 @@
-import React from 'react';
-import './style.css';
+import React, {Component} from 'react';
 
-
-const Body = (props) => {
-
-  return(
-    
-    <body id ="bd">
-      <hr/>
-        <h5>Welcome to the Hell {props.name}</h5>
-      <hr/>
-    </body>
-    
-    );
-
-}
-
-const Header = () => {
-return(
-  <nav id="menu">
-      <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Produtos</a></li>
-          <li><a href="#">Missão</a></li>
-          <li><a href="#">Links</a></li>
-          <li><a href="#">Contato</a></li>
-      </ul>
-  </nav>
- );
-}
-
-
-function App() {
-  return(
-    <div>
-      <Header/>
-      <Body name="Bruce Wayne" />
-    </div>
-  );
+class App extends Component {
+    render(){
+        return(
+            <div>
+                Welcome to the hell!
+            </div>
+        )
+    }
 }
 
 export default App;
